89c4f6eb83597c7dfc87c04eaf8fc15a425bf803
sorlovsky <simon.orlovsky@gmail.com>
2017-02-15 21:08:34 -0600

    camera added

--- app/src/main/java/com/crystal_ar/crystalcompsdemo/TextActivity.java
+++ app/src/main/java/com/crystal_ar/crystalcompsdemo/TextActivity.java
@@ -1,13 +1,40 @@
 package com.crystal_ar.crystalcompsdemo;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class TextActivity extends AppCompatActivity {
+
+    private static final int CAMERA_REQUEST = 1888;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text);
+
+        this.imageView = (ImageView)this.findViewById(R.id.textImageView);
+        Button photoButton = (Button) this.findViewById(R.id.BtnTakePhoto);
+        photoButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
     }
 }
