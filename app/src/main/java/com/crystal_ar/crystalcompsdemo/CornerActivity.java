e3b5f514e9b300faa727315dd4834eb9488e0b45
Frederik Stensaeth <stensaethf@carleton.edu>
2017-02-16 19:55:37 -0600

    added imageview

--- app/src/main/java/com/crystal_ar/crystalcompsdemo/CornerActivity.java
+++ app/src/main/java/com/crystal_ar/crystalcompsdemo/CornerActivity.java
@@ -1,8 +1,13 @@
 package com.crystal_ar.crystalcompsdemo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.crystal_ar.crystal_ar.CrystalAR;
 
@@ -14,9 +19,22 @@ public class CornerActivity extends AppCompatActivity {
 
     private ImageView imageView;
     private CrystalAR crystalAR;
+    private LinearLayout linearLayout;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_corner);
+
+        this.imageView = (ImageView) findViewById(R.id.cornerImageView);
+        this.imageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN){
+                    // logs the x,y coordinates in the monitor.
+                    Log.d("COORDINATE", String.valueOf(event.getX()) + "x" + String.valueOf(event.getY()));
+                }
+                return true;
+            }
+        });
     }
 }
