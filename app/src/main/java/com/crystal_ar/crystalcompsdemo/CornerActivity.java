5c956dbe029a6d3de35794a0c4ef14d966b3c5a5
Frederik Stensaeth <stensaethf@carleton.edu>
2017-02-19 23:40:41 -0600

    added take/load photo buttons

--- app/src/main/java/com/crystal_ar/crystalcompsdemo/CornerActivity.java
+++ app/src/main/java/com/crystal_ar/crystalcompsdemo/CornerActivity.java
@@ -1,15 +1,80 @@
 package com.crystal_ar.crystalcompsdemo;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.crystal_ar.crystal_ar.CrystalAR;
 
 /**
  * Created by Frederik on 2/19/17.
  */
 
 public class CornerActivity extends AppCompatActivity {
+
+    private static final int CAMERA_REQUEST = 1888;
+    private static final int RESULT_LOAD_IMAGE = 1;
+
+    private CrystalAR crystalAR;
+    private ImageView imageView;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_corner);
+
+        this.imageView = (ImageView)this.findViewById(R.id.cornerImageView);
+
+        Button takePhotoButton = (Button) this.findViewById(R.id.BtnTakePhoto);
+        takePhotoButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, CAMERA_REQUEST);
+            }
+        });
+
+        Button loadPhotoButton = (Button) this.findViewById(R.id.BtnLoadPhoto);
+        loadPhotoButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, RESULT_LOAD_IMAGE);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        } else if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+        }
     }
 }
